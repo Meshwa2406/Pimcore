dddb61476501560722f475ce85485f06b2330d1e
JiaJia Ji <kingjia90@gmail.com>
2024-12-02 10:14:34 +0100

    don't know about disableBlockClearing usages, keeping it for the moment

--- bundles/CoreBundle/src/EventListener/Frontend/BlockStateListener.php
+++ bundles/CoreBundle/src/EventListener/Frontend/BlockStateListener.php
@@ -56,10 +56,9 @@ class BlockStateListener implements EventSubscriberInterface, LoggerAwareInterfa
             return;
         }
 
-        // Can be removed? Seems some legacy leftover
-        //        if ($request->get('disableBlockClearing')) {
-        //            return;
-        //        }
+        if ($request->query->get('disableBlockClearing') || $request->request->get('disableBlockClearing')) {
+           return;
+        }
 
         // main request already has a state on the stack
         if ($event->isMainRequest()) {
@@ -79,9 +78,9 @@ class BlockStateListener implements EventSubscriberInterface, LoggerAwareInterfa
             return;
         }
 
-        //        if ($request->get('disableBlockClearing')) {
-        //            return;
-        //        }
+        if ($request->query->get('disableBlockClearing') || $request->request->get('disableBlockClearing')) {
+           return;
+        }
 
         if ($this->blockStateStack->count() > 1) {
             // restore parent block data by removing sub-request block state
