Meshwa2406/Pimcore

commit b0b17d4eb0f0f6d813dc0b771de951d9e3c24224

[Improvement]: Replace Request::get with explicit input sources (#17873)
* replace request get with explicit source

* Apply php-cs-fixer changes

* fix stan

* fix codecept?

* fix codecept

* apply suggestions

Co-authored-by: Jacob Dreesen <j.dreesen@neusta.de>

* Apply php-cs-fixer changes

* revert unwanted change

* don't know about disableBlockClearing usages, keeping it for the moment

* Apply php-cs-fixer changes

---------

Co-authored-by: kingjia90 <kingjia90@users.noreply.github.com>
Co-authored-by: Jacob Dreesen <j.dreesen@neusta.de>

lib/Http/Request/Resolver/TemplateResolver.php
@@ -30,7 +30,7 @@ class TemplateResolver extends AbstractRequestResolver
             $request = $this->getCurrentRequest();
         }
 
-        return $request->get(DynamicRouter::CONTENT_TEMPLATE);
+        return $request->attributes->getString(DynamicRouter::CONTENT_TEMPLATE);
     }
 
     public function setTemplate(Request $request, string $template): void

bundles/ApplicationLoggerBundle/src/Controller/LogController.php
@@ -54,14 +54,30 @@ class LogController extends UserAwareController implements KernelControllerEvent
      */
     public function showAction(Request $request, Connection $db): JsonResponse
     {
+        $requestSource = $request->request;
+
+        //TODO: Remove the GET method support in Pimcore 12
+        if ($request->isMethod('GET')) {
+            trigger_deprecation(
+                'pimcore/pimcore',
+                '11.5.0',
+                sprintf('Calling route "%s" (%s) via "GET" method is deprecated and will not be supported anymore in 12.
+                Please use "POST" method instead.',
+                    'pimcore_admin_bundle_applicationlogger_log_show',
+                    __METHOD__
+                )
+            );
+            $requestSource = $request->query;
+        }
+
         $this->checkPermission('application_logging');
 
         $qb = $db->createQueryBuilder();
         $qb
             ->select('*')
             ->from(ApplicationLoggerDb::TABLE_NAME)
-            ->setFirstResult($request->get('start', 0))
-            ->setMaxResults($request->get('limit', 50));
+            ->setFirstResult($requestSource->getInt('start', 0))
+            ->setMaxResults($requestSource->getInt('limit', 50));
 
         $qb->orderBy('id', 'DESC');
 
@@ -76,35 +92,35 @@ class LogController extends UserAwareController implements KernelControllerEvent
             }
         }
 
-        $priority = $request->get('priority');
+        $priority = $requestSource->getString('priority');
         if (!empty($priority)) {
             $qb->andWhere($qb->expr()->eq('priority', ':priority'));
             $qb->setParameter('priority', $priority);
         }
 
-        if ($fromDate = $this->parseDateObject($request->get('fromDate'), $request->get('fromTime'))) {
+        if ($fromDate = $this->parseDateObject($requestSource->getString('fromDate'), $requestSource->getString('fromTime'))) {
             $qb->andWhere('timestamp > :fromDate');
             $qb->setParameter('fromDate', $fromDate, Types::DATETIME_MUTABLE);
         }
 
-        if ($toDate = $this->parseDateObject($request->get('toDate'), $request->get('toTime'))) {
+        if ($toDate = $this->parseDateObject($requestSource->getString('toDate'), $requestSource->getString('toTime'))) {
             $qb->andWhere('timestamp <= :toDate');
             $qb->setParameter('toDate', $toDate, Types::DATETIME_MUTABLE);
         }
 
-        if (!empty($component = $request->get('component'))) {
+        if (!empty($component = $requestSource->getString('component'))) {
             $qb->andWhere('component = ' . $qb->createNamedParameter($component));
         }
 
-        if (!empty($relatedObject = $request->get('relatedobject'))) {
+        if (!empty($relatedObject = $requestSource->getString('relatedobject'))) {
             $qb->andWhere('relatedobject = ' . $qb->createNamedParameter($relatedObject));
         }
 
-        if (!empty($message = $request->get('message'))) {
+        if (!empty($message = $requestSource->getString('message'))) {
             $qb->andWhere('message LIKE ' . $qb->createNamedParameter('%' . $message . '%'));
         }
 
-        if (!empty($pid = $request->get('pid'))) {
+        if (!empty($pid = $requestSource->getInt('pid'))) {
             $qb->andWhere('pid LIKE ' . $qb->createNamedParameter('%' . $pid . '%'));
         }
 
@@ -210,7 +226,7 @@ class LogController extends UserAwareController implements KernelControllerEvent
     {
         $this->checkPermission('application_logging');
 
-        $filePath = $request->get('filePath');
+        $filePath = $request->query->getString('filePath');
         $storage = Storage::get('application_log');
 
         if ($storage->fileExists($filePath)) {

bundles/CoreBundle/src/Controller/PublicServicesController.php
@@ -35,14 +35,14 @@ class PublicServicesController extends Controller
 {
     public function thumbnailAction(Request $request): RedirectResponse|StreamedResponse
     {
-        $filename = $request->get('filename');
+        $filename = $request->attributes->getString('filename');
         $requestedFileExtension = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
 
         $config = [
-            'prefix' => $request->get('prefix', ''),
-            'type' => $request->get('type'),
-            'asset_id' => (int) $request->get('assetId'),
-            'thumbnail_name' => $request->get('thumbnailName'),
+            'prefix' => $request->attributes->getString('prefix', ''),
+            'type' => $request->attributes->getString('type'),
+            'asset_id' => $request->attributes->getInt('assetId'),
+            'thumbnail_name' => $request->attributes->getString('thumbnailName'),
             'filename' => $filename,
             'file_extension' => $requestedFileExtension,
         ];

bundles/CoreBundle/src/EventListener/Frontend/BlockStateListener.php
@@ -56,7 +56,7 @@ class BlockStateListener implements EventSubscriberInterface, LoggerAwareInterfa
             return;
         }
 
-        if ($request->get('disableBlockClearing')) {
+        if ($request->query->get('disableBlockClearing') || $request->request->get('disableBlockClearing')) {
             return;
         }
 
@@ -78,7 +78,7 @@ class BlockStateListener implements EventSubscriberInterface, LoggerAwareInterfa
             return;
         }
 
-        if ($request->get('disableBlockClearing')) {
+        if ($request->query->get('disableBlockClearing') || $request->request->get('disableBlockClearing')) {
             return;
         }
 

bundles/CoreBundle/src/EventListener/Frontend/ElementListener.php
@@ -108,8 +108,9 @@ class ElementListener implements EventSubscriberInterface, LoggerAwareInterface
 
     protected function handleVersion(Request $request, Document $document): Document
     {
-        if ($v = $request->get('v')) {
-            if ($version = Version::getById((int) $v)) {
+        $v = $request->query->getInt('v');
+        if ($v) {
+            if ($version = Version::getById($v)) {
                 if ($version->getPublic()) {
                     $this->logger->info('Setting version to {version} for document {document}', [
                         'version' => $version->getId(),
@@ -120,7 +121,7 @@ class ElementListener implements EventSubscriberInterface, LoggerAwareInterface
                 }
             } else {
                 $this->logger->notice('Failed to load {version} for document {document}', [
-                    'version' => $request->get('v'),
+                    'version' => $v,
                     'document' => $document->getFullPath(),
                 ]);
             }
@@ -141,7 +142,7 @@ class ElementListener implements EventSubscriberInterface, LoggerAwareInterface
         }
 
         // document preview
-        if ($request->get('pimcore_preview')) {
+        if ($request->query->getBoolean('pimcore_preview')) {
             // get document from session
 
             // TODO originally, this was the following call. What was in this->getParam('document') and
@@ -158,9 +159,10 @@ class ElementListener implements EventSubscriberInterface, LoggerAwareInterface
         }
 
         // for version preview
-        if ($request->get('pimcore_version')) {
+        if ($request->query->has('pimcore_version')) {
+            $versionId = $request->query->getInt('pimcore_version');
             // TODO there was a check with a registry flag here - check if the main request handling is sufficient
-            $version = Version::getById((int) $request->get('pimcore_version'));
+            $version = Version::getById($versionId);
             if ($documentVersion = $version?->getData()) {
                 $document = $documentVersion;
                 $this->logger->debug('Loading version {version} for document {document} from pimcore_version parameter', [
@@ -169,13 +171,13 @@ class ElementListener implements EventSubscriberInterface, LoggerAwareInterface
                 ]);
             } else {
                 $this->logger->warning('Failed to load {version} for document {document} from pimcore_version parameter', [
-                    'version' => $request->get('pimcore_version'),
+                    'version' => $$versionId,
                     'document' => $document->getFullPath(),
                 ]);
 
                 throw new NotFoundHttpException(
                     sprintf('Failed to load %s for document %s from pimcore_version parameter',
-                        $request->get('pimcore_version'), $document->getFullPath()));
+                        $versionId, $document->getFullPath()));
             }
         }
 
@@ -215,7 +217,7 @@ class ElementListener implements EventSubscriberInterface, LoggerAwareInterface
     protected function handleObjectParams(Request $request): void
     {
         // object preview
-        if ($objectId = $request->get('pimcore_object_preview')) {
+        if ($objectId = $request->query->getInt('pimcore_object_preview')) {
             if ($object = Service::getElementFromSession('object', $objectId, $request->getSession()->getId())) {
                 $this->logger->debug('Loading object {object} ({objectId}) from session', [
                     'object' => $object->getFullPath(),

bundles/CoreBundle/src/EventListener/Traits/PreviewRequestTrait.php
@@ -33,7 +33,7 @@ trait PreviewRequestTrait
             return true;
         }
 
-        if ($request->get('pimcore_preview') === 'true') {
+        if ($request->query->getBoolean('pimcore_preview')) {
             return true;
         }
 

bundles/CustomReportsBundle/src/Controller/Reports/CustomReportController.php
@@ -78,9 +78,10 @@ class CustomReportController extends UserAwareController
 
         $success = false;
 
-        $this->isValidConfigName($request->get('name'));
+        $reportName = $request->request->getString('name');
+        $this->isValidConfigName($reportName);
 
-        $report = Tool\Config::getByName($request->get('name'));
+        $report = Tool\Config::getByName($reportName);
 
         if (!$report) {
             $report = new Tool\Config();
@@ -88,7 +89,7 @@ class CustomReportController extends UserAwareController
                 throw new ConfigWriteException();
             }
 
-            $report->setName($request->get('name'));
+            $report->setName($reportName);
             $report->save();
 
             $success = true;
@@ -106,7 +107,7 @@ class CustomReportController extends UserAwareController
     {
         $this->checkPermission('reports_config');
 
-        $report = Tool\Config::getByName($request->get('name'));
+        $report = Tool\Config::getByName($request->request->getString('name'));
         if (!$report) {
             throw $this->createNotFoundException();
         }
@@ -128,14 +129,14 @@ class CustomReportController extends UserAwareController
     {
         $this->checkPermission('reports_config');
 
-        $newName = $request->get('newName');
+        $newName = $request->request->getString('newName');
         $this->isValidConfigName($newName);
         $report = Tool\Config::getByName($newName);
         if ($report) {
             throw new Exception('report already exists');
         }
 
-        $report = Tool\Config::getByName($request->get('name'));
+        $report = Tool\Config::getByName($request->request->getString('name'));
         if (!$report) {
             throw $this->createNotFoundException();
         }
@@ -166,7 +167,7 @@ class CustomReportController extends UserAwareController
     {
         $this->checkPermissionsHasOneOf(['reports_config', 'reports']);
 
-        $report = Tool\Config::getByName($request->get('name'));
+        $report = Tool\Config::getByName($request->query->getString('name'));
         if (!$report) {
             throw $this->createNotFoundException();
         }
@@ -184,8 +185,9 @@ class CustomReportController extends UserAwareController
     public function updateAction(Request $request): JsonResponse
     {
         $this->checkPermission('reports_config');
-        $this->isValidConfigName($request->get('name'));
-        $report = Tool\Config::getByName($request->get('name'));
+        $reportName = $request->request->getString('name');
+        $this->isValidConfigName($reportName);
+        $report = Tool\Config::getByName($reportName);
         if (!$report) {
             throw $this->createNotFoundException();
         }
@@ -193,7 +195,7 @@ class CustomReportController extends UserAwareController
             throw new ConfigWriteException();
         }
 
-        $data = $this->decodeJson($request->get('configuration'));
+        $data = $this->decodeJson($request->request->getString('configuration'));
 
         if (!is_array($data['yAxis'])) {
             $data['yAxis'] = strlen($data['yAxis'] ?? '') ? [$data['yAxis']] : [];
@@ -220,13 +222,13 @@ class CustomReportController extends UserAwareController
     {
         $this->checkPermission('reports_config');
 
-        $report = Tool\Config::getByName($request->get('name'));
+        $report = Tool\Config::getByName($request->request->getString('name'));
         if (!$report) {
             throw $this->createNotFoundException();
         }
         $columnConfiguration = $report->getColumnConfiguration();
 
-        $configuration = json_decode($request->get('configuration'));
+        $configuration = json_decode($request->request->getString('configuration'));
         $configuration = $configuration[0] ?? null;
 
         $success = false;
@@ -306,9 +308,9 @@ class CustomReportController extends UserAwareController
         if (!class_exists(\Pimcore\Bundle\AdminBundle\Helper\QueryParams::class)) {
             throw new AdminClassicBundleNotFoundException('This action requires package "pimcore/admin-ui-classic-bundle" to be installed.');
         }
-        $offset = (int) $request->get('start', 0);
-        $limit = (int) $request->get('limit', 40);
-        $config = Tool\Config::getByName($request->get('name'));
+        $offset = $request->request->getInt('start', 0);
+        $limit = $request->request->getInt('limit', 40);
+        $config = Tool\Config::getByName($request->request->getString('name'));
         if (!$config) {
             throw $this->createNotFoundException();
         }
@@ -333,18 +335,18 @@ class CustomReportController extends UserAwareController
     {
         $this->checkPermission('reports');
 
-        $field = $request->get('field');
-        $filters = ($request->get('filter') ? json_decode($request->get('filter'), true) : null);
-        $drillDownFilters = $request->get('drillDownFilters', null);
+        $field = $request->request->getString('field');
+        $filters = ($request->request->getString('filter') ? json_decode($request->request->getString('filter'), true) : null);
+        $drillDownFilters = $request->request->all('drillDownFilters');
 
-        $config = Tool\Config::getByName($request->get('name'));
+        $config = Tool\Config::getByName($request->request->getString('name'));
         if (!$config) {
             throw $this->createNotFoundException();
         }
         $configuration = $config->getDataSourceConfig();
 
         $adapter = Tool\Config::getAdapter($configuration, $config);
-        $result = $adapter->getAvailableOptions($filters ?? [], $field ?? '', $drillDownFilters ?? []);
+        $result = $adapter->getAvailableOptions($filters ?? [], $field, $drillDownFilters);
 
         return $this->jsonResponse([
             'success' => true,
@@ -360,7 +362,7 @@ class CustomReportController extends UserAwareController
     public function chartAction(Request $request): JsonResponse
     {
         $this->checkPermission('reports');
-        $config = Tool\Config::getByName($request->get('name'));
+        $config = Tool\Config::getByName($request->request->getString('name'));
         if (!$config) {
             throw $this->createNotFoundException();
         }
@@ -397,16 +399,16 @@ class CustomReportController extends UserAwareController
 
         set_time_limit(300);
 
-        $sort = $request->get('sort');
-        $dir = $request->get('dir');
-        $filters = $request->get('filter') ? json_decode(urldecode($request->get('filter')), true) : null;
-        $drillDownFilters = $request->get('drillDownFilters', null);
+        $sort = $request->query->getString('sort');
+        $dir = $request->query->getString('dir');
+        $filters = $request->query->has('filter') ? json_decode(urldecode($request->query->getString('filter')), true) : null;
+        $drillDownFilters = $request->query->getString('drillDownFilters');
         if ($drillDownFilters) {
             $drillDownFilters = json_decode($drillDownFilters, true);
         }
         $includeHeaders = $request->query->getBoolean('headers');
 
-        $config = Tool\Config::getByName($request->get('name'));
+        $config = Tool\Config::getByName($request->query->getString('name'));
         if (!$config) {
             throw $this->createNotFoundException();
         }
@@ -423,13 +425,12 @@ class CustomReportController extends UserAwareController
 
         $adapter = Tool\Config::getAdapter($configuration, $config);
 
-        $offset = $request->get('offset', 0);
+        $offset = $request->query->getInt('offset');
         $limit = 5000;
-        $tempData = [];
         $result = $adapter->getData($filters, $sort, $dir, $offset * $limit, $limit, $fields, $drillDownFilters);
         ++$offset;
 
-        if (!($exportFile = $request->get('exportFile'))) {
+        if (!($exportFile = $request->query->getString('exportFile'))) {
             $exportFile = PIMCORE_SYSTEM_TEMP_DIRECTORY . '/report-export-' . uniqid() . '.csv';
             @unlink($exportFile);
         } else {
@@ -468,7 +469,7 @@ class CustomReportController extends UserAwareController
     public function downloadCsvAction(Request $request): BinaryFileResponse
     {
         $this->checkPermission('reports');
-        if ($exportFile = $request->get('exportFile')) {
+        if ($exportFile = $request->query->getString('exportFile')) {
             $exportFile = $this->getTemporaryFileFromFileName($exportFile);
             $response = new BinaryFileResponse($exportFile);
             $response->headers->set('Content-Type', 'text/csv; charset=UTF-8');
@@ -504,8 +505,8 @@ class CustomReportController extends UserAwareController
             $sort = $sortingSettings['orderKey'];
             $dir = $sortingSettings['order'];
         }
-        $filters = ($request->get('filter') ? json_decode($request->get('filter'), true) : null);
-        $drillDownFilters = $request->get('drillDownFilters', null);
+        $filters = ($request->request->has('filter') ? json_decode($request->request->getString('filter'), true) : null);
+        $drillDownFilters = $request->request->all('drillDownFilters');
         if ($sort === null && $dir === null && property_exists($configuration, 'orderby') && $configuration->orderby !== '' && $configuration->orderbydir !== '') {
             $sort = $configuration->orderby;
             $dir = $configuration->orderbydir;

bundles/GlossaryBundle/src/Controller/SettingsController.php
@@ -45,20 +45,18 @@ class SettingsController extends UserAwareController
         // check glossary permissions
         $this->checkPermission('glossary');
 
-        if ($request->get('data')) {
+        if ($request->request->has('data')) {
+            $data = $this->decodeJson($request->request->getString('data'));
 
             Cache::clearTag('glossary');
 
-            if ($request->get('xaction') === 'destroy') {
-                $data = $this->decodeJson($request->get('data'));
+            if ($request->query->getString('xaction') === 'destroy') {
                 $id = $data['id'];
                 $glossary = Glossary::getById($id);
                 $glossary->delete();
 
                 return $this->jsonResponse(['success' => true, 'data' => []]);
-            } elseif ($request->get('xaction') === 'update') {
-                $data = $this->decodeJson($request->get('data'));
-
+            } elseif ($request->query->getString('xaction') === 'update') {
                 // save glossary
                 $glossary = Glossary::getById($data['id']);
 
@@ -81,8 +79,7 @@ class SettingsController extends UserAwareController
                 }
 
                 return $this->jsonResponse(['data' => $glossary, 'success' => true]);
-            } elseif ($request->get('xaction') == 'create') {
-                $data = $this->decodeJson($request->get('data'));
+            } elseif ($request->query->getString('xaction') == 'create') {
                 unset($data['id']);
 
                 // save glossary
@@ -114,8 +111,8 @@ class SettingsController extends UserAwareController
             }
 
             $list = new Glossary\Listing();
-            $list->setLimit((int) $request->get('limit', 50));
-            $list->setOffset((int) $request->get('start', 0));
+            $list->setLimit($request->request->getInt('limit', 50));
+            $list->setOffset($request->request->getInt('start'));
 
             $sortingSettings = \Pimcore\Bundle\AdminBundle\Helper\QueryParams::extractSortingSettings(array_merge($request->request->all(), $request->query->all()));
             if ($sortingSettings['orderKey']) {
@@ -123,8 +120,8 @@ class SettingsController extends UserAwareController
                 $list->setOrder($sortingSettings['order']);
             }
 
-            if ($request->get('filter')) {
-                $list->setCondition('`text` LIKE ' . $list->quote('%'.$request->get('filter').'%'));
+            if ($request->request->has('filter')) {
+                $list->setCondition('`text` LIKE ' . $list->quote('%'.$request->request->getString('filter').'%'));
             }
 
             $list->load();

commit 935e88c47660f7517940d0c07d8d706abda29934

Fixing regression caused by #17873 in TemplateResolver

lib/Http/Request/Resolver/TemplateResolver.php
@@ -30,7 +30,7 @@ class TemplateResolver extends AbstractRequestResolver
             $request = $this->getCurrentRequest();
         }
 
-        return $request->attributes->getString(DynamicRouter::CONTENT_TEMPLATE);
+        return $request->get(DynamicRouter::CONTENT_TEMPLATE);
     }
 
     public function setTemplate(Request $request, string $template): void